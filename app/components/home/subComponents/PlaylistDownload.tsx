fd2f38f561ffc7ad611dce8a6cf6f19d734eb5fd
tulshi das <bsse0811@iit.du.ac.bd>
2021-02-21 11:05:57 +0000

    remove comments

--- app/components/home/subComponents/PlaylistDownload.tsx
+++ app/components/home/subComponents/PlaylistDownload.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { IPlaylistDownloadState } from "../states";
-import { FaFolderOpen, FaAngleDown, FaAngleUp, FaSortAmountDown } from "react-icons/fa";
+import { FaAngleDown, FaAngleUp, FaSortAmountDown } from "react-icons/fa";
 import { ipcRenderer } from "electron";
 import { Renderer_Events, Main_Events } from "../../../constants/constants";
 import { SingleVideo } from "./SingleVideo";
@@ -95,97 +95,8 @@ export class PlaylistDownload extends React.PureComponent<IPlaylistDownloadProps
     if(!this.state.info) return;
     let downloadingIndex = this.state.info.items.findIndex(x=>x.id === this.state.downloadingItem?.id);
     downloadingIndex++;
-    this.setState({downloadingItem:this.state.info.items[downloadingIndex]});
-    // if(this.state.currentDownloadIndex >= this.state.info?.items.length! - 1) return;
-
-    // var interval = setInterval(()=>{
-    //   if(!this.state.info) return;
-    //   const downloadingIndex = this.state.info.items.findIndex(x=>x.id === this.state.downloadingItem?.id);
-    //   const fetchingIndex = this.state.info?.items.findIndex(x=>x.id === this.state.fetchingItem?.id);
-    //   if(downloadingIndex < fetchingIndex) {
-    //     this.setState({currentDownloadIndex:this.state.currentDownloadIndex+ 1},this.downloadVideo);
-    //     clearInterval(interval);
-    //   }
-    // },1000)
-
+    this.setState({downloadingItem:this.state.info.items[downloadingIndex]});    
   }
-  // handleFolderClick=()=>{
-  //   ipcRenderer.send(Renderer_Events.OPEN_FOLDER,this.props.downloadInfo.playList?.downloadPath!);
-  // }
-
-  // fetchVideoInfo=()=>{
-  //   console.log('fetching index:'+this.fetchIndex);
-  //   const videoId = this.props.downloadInfo.playList?.info.items[this.fetchIndex].id!
-  //   const data:IFetch={
-  //     channel:Main_Events.ON_SINGLE_VIDEO_INFO_FETCH_COMPLETE+this.props.downloadInfo.id,
-  //     videoId:videoId,
-  //     playlistPath:this.props.downloadInfo.playList?.downloadPath
-  //   }
-  //   ipcRenderer.send(Renderer_Events.FETCH_SINGLE_VIDEO_INFO,data);
-  // }
-
-  // handleFetchComplete=()=>{
-  //   ipcRenderer.on(this.channels.onFetchVideo,(_e, data: ISingleVideo)=>{
-  //     console.log('fetch complete');
-  //     this.setState({
-  //       videoList:[...this.state.videoList,data]
-  //     },()=>{
-  //       if(this.state.currentDownloadIndex === -1){
-  //         console.log('starting first downloading');
-  //         console.log(this.state);
-  //         this.downloadNextVideo();
-  //       }
-  //     })
-  //     this.fetchIndex++;
-  //     if(this.fetchIndex < this.props.downloadInfo.playList?.info.items.length!) this.fetchVideoInfo()
-  //   })
-  // }
-
-  // handleFetchTimeLimitExit=()=>{
-  //   var prevFetchIndex = this.fetchIndex;
-  //   var interval = setInterval(()=>{
-  //     if(this.fetchIndex >= this.props.downloadInfo.playList?.info.items.length! -1){
-  //       clearInterval(interval);
-  //       return;
-  //     }
-  //     if(prevFetchIndex === this.fetchIndex){
-  //       console.log('time limit exit');
-  //       this.fetchIndex++;
-  //       this.fetchVideoInfo();
-  //     }
-  //     else prevFetchIndex=this.fetchIndex;
-  //   },this.timeLimitForFetch)
-  // }
-
-  // downloadVideo=()=>{
-  //   // console.log('fetchIndex:'+this.fetchIndex)
-  //   console.log('downloadIndex:'+this.state.currentDownloadIndex);
-  //   const data:IPlaylistVideo={
-  //     playlistId:this.props.downloadInfo.id,
-  //     video:this.state.videoList[this.state.currentDownloadIndex]
-  //   }
-  //   ipcRenderer.send(Renderer_Events.DOWNLOAD_PLALIST_VIDEO,data);
-  // }
-
-  // handleDownloadTimeLimitExit=()=>{
-  //   var prevDownloadIndex = this.state.currentDownloadIndex;
-  //   var interval = setInterval(()=>{
-  //     if(this.state.currentDownloadIndex >= this.props.downloadInfo.playList?.info.items.length! -1){
-  //       clearInterval(interval);
-  //       return;
-  //     }
-  //     if(prevDownloadIndex === this.state.currentDownloadIndex){
-  //       console.log('download time limit exit');
-  //       this.downloadNextVideo();
-  //     }
-  //     else prevDownloadIndex=this.state.currentDownloadIndex;
-  //   },this.timeLimitForDownload)
-  // }
-
-  // handleTimeLimitExit=()=>{
-  //   this.handleFetchTimeLimitExit();
-  //   this.handleDownloadTimeLimitExit();
-  // }
 
   fetchPlaylistInfo=()=>{
     ipcRenderer.send(Renderer_Events.FETCH_PLAYLIST_INFO ,this.props.id);
