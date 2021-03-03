a5179ce10d8cdc2ea0dd44db2d885497ef94f3e1
tulshi das <bsse0811@iit.du.ac.bd>
2021-03-04 00:13:33 +0600

    initial commit

--- app/lib/helpers.ts
+++ app/lib/helpers.ts
@@ -10,3 +10,10 @@ export class Helper{
   }
 
 }
+export class FB_Helper{
+  //a valide video link: https://www.facebook.com/facebookapp/videos/10153231379946729/
+  
+  static getFbVideoId(url:string){
+    //669354717351221
+  }
+}
