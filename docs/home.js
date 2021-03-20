b34dc2a1c3eb4c7366991f85a283bfca21d12cb7
tulshi das <bsse0811@iit.du.ac.bd>
2021-03-20 12:22:14 +0000

    integrate react umd and basic html with best practice

---
+++ docs/home.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+function HomeComponent() {
+    return (
+      <div className="col-12">
+        <h6>An Youtube video and playlist downloader application</h6>
+        <h3>Features</h3>
+        <ul>
+          <li>Youtube video download</li>
+          <li>Youtube playlist download</li>
+          <li>Download video in audio format or different format and qulity</li>
+          <li>Download Selected videos from playlist</li>
+          <li>Download parallelly playlist videos</li>
+        </ul>
+        <div id="download" className="download"></div>
+      </div>
+    );  
+}
+
+const Home = React.memo(HomeComponent);
+
+const domContainer = document.querySelector('#home');
+ReactDOM.render(e(Home), domContainer);
