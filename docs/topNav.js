aba29fd351cd858f00eec527c67ea2a066f8570a
tulshi das <bsse0811@iit.du.ac.bd>
2021-03-21 08:05:20 +0000

    fix url path problem

--- docs/topNav.js
+++ docs/topNav.js
@@ -3,19 +3,19 @@
 const e = React.createElement;
 
 function TopNavComponent() {
-  const path = window.location.pathname;
-  console.log('path',path);
+  const url = window.location.href;
+  console.log('path',url);
     return (
       <React.Fragment>
         <div className="col-4">          
           <span className="text-white h4 m-0">
-            <a href="/" className="text-white text-decoration-none">Kr-youtube-downloader</a> 
+            <a href={BaseUrl} className="text-white text-decoration-none">Kr-youtube-downloader</a>
           </span>
         </div>
         <div className="col-4">
           <span>Docs</span>
         </div>
-        <div className={`col-4 nav-item-download ${(path === '/download' || path === '/download.html')?'active':''}`}>
+        <div className={`col-4 nav-item-download ${(url === Urls.Download || url === Urls.Download+".html")?'active':''}`}>
           <a href="./download.html" className="text-white text-decoration-none"><span>Download</span></a> 
         </div>
       </React.Fragment>
