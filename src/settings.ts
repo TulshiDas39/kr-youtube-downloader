207adaa1104d69ae32743d569844f7a1e9f75f65
tulshi das <bsse0811@iit.du.ac.bd>
2023-10-18 23:30:25 +0600

    update package scripts

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export class Settings{
-    static readonly ENV:'DEVELOPMENT' | 'PRODUCTION' = 'DEVELOPMENT';
+    static readonly ENV:'DEVELOPMENT' | 'PRODUCTION' = 'PRODUCTION';
     static readonly FRONTEND_PORT = 54533;
 }
