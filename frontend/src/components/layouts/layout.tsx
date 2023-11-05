8bb287c20e0fb73b9377e90a2c58a2a75639ddd5
tulshi das <bsse0811@iit.du.ac.bd>
2023-11-05 16:46:09 +0600

    fix UI

--- frontend/src/components/layouts/layout.tsx
+++ frontend/src/components/layouts/layout.tsx
@@ -1,16 +1,13 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Route, Routes  } from 'react-router-dom';
 import './layout.scss'
 import { UiRoutes } from '../../lib/UiRoutes';
 import { Main } from '..';
 
 function LayoutComponent() {
-    const data = useRef({topHeighPercent:10});
     return (
-        <div id="layout" className="d-flex flex-column h-100">
-            <div className="d-flex" style={{height:`${data.current.topHeighPercent}%`}}>
-            </div>
-            <div className="" style={{height:`${100 - data.current.topHeighPercent}%`}}>
+        <div id="layout" className="d-flex flex-column h-100">            
+            <div className="" style={{height:`${100}%`}}>
                 <Routes >
                     <Route path={UiRoutes.Root} element={<Main />} />
                 </Routes >
